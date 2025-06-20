8de7fa7d52a275f5c8ecb4dfb757613779de8897
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-06-20 16:10:19 +0000

    Worked on string manipulation

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ two = 1234567890
 three = True
  
 print(f"This is a {type(one)}, I am a {type(two)} and finally I am a {type(three)}")
+
+# String manipulation 
+
+str_one = "Hi My Name Is Patricia."
+multiple_str = str_one * 10 
+upper_str = str_one.upper()
+capital_str = str_one.capitalize()
+capital_each_word = str_one.title()
+find_letter_index = str_one.find("M")
+print(find_letter_index)
