c6206cafb7c5b89a3a10633a325d39e2d65855dc
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-06-20 15:54:28 +0000

    Type checking

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ print("Hello World")
 
 greet = "This is a function that is saying hello"
 print(greet)
+
+# Type Checking 
+
+one = "Bonjour"
+two = 1234567890
+three = True
+ 
+print(f"This is a {type(one)}, I am a {type(two)} and finally I am a {type(three)}")
