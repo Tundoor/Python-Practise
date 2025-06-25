8865cbf64fe96d6125a88637dd1348c3cfd4f0a9
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-06-25 14:02:30 +0000

    Comparisons and if statements

--- main.py
+++ main.py
@@ -70,3 +70,19 @@ for char in text.lower():
 
 # a += b
 # The addition assignment operator enables you to add a value to a variable and then assign the result to that variable.
+
+
+# Comparison operators allow you to compare two objects based on their values. You can use a comparison operator by placing it between the objects you want to compare. They return a Boolean value — namely True or False — depending on the truthfulness of the expression.
+
+# Python has the following comparison operators:
+
+# Operator	Meaning
+# ==	Equal
+# !=	Not equal
+# >	Greater than
+# <	Less than
+# >=	Greater than or equal to
+# <=	Less than or equal to
+
+if x != 0:
+    print(x) # This is the format of an if statement
