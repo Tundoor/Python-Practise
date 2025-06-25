52a4470103e9349da6f80cb08ceb371e03ecfb92
Thandolwethu Mbokazi <thandwem045@gmail.com>
2025-06-25 13:19:06 +0000

    Understanding finding indices

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ capital_str = str_one.capitalize()
 capital_each_word = str_one.title()
 find_letter_index = str_one.find("M")
 print(find_letter_index)
+
+
+## Free Code Camp 
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+index = alphabet.find(text[0].lower())  ## Finds the h in hello World in our alphabet varieble
+print(index)
+shifted = alphabet[index + shift ]  ### Square brackets finds the letter at a certain index. Find gets us said index 
+print(shifted) 
