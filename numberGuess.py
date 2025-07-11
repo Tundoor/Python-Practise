8997c9d198bcc210176fceff5434d415f2d2d86a
Thando <thandwem045@gmail.com>
2025-07-11 23:57:14 +0200

    Created numberGuess.py

---
+++ numberGuess.py
@@ -0,0 +1,12 @@
+# 🐍 Beginner Project: Number Guessing Game
+# 🎯 Goal:
+# Let the user guess a number between 1 and 100. The program gives hints if the guess is too high or too low until the correct number is guessed.
+
+# what we need:
+    
+#     1: Generate a random number between 1 and 100.
+#    2: Ask the user to guess the number.
+#    3: Provide feedback on whether the guess is too high, too low, or correct.
+
+user_input = input("Guess a number between 0 and 100")
+print(user_input)
